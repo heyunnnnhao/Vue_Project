11f17f060d94d81620efd31a1de1ced8fb8f6a6a
heyunnnnhao <heyunhaotq@yahoo.com>
2020-02-05 14:41:53 -0500

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js Apppppp"/>
+    <HelloWorld msg="Welcome to Yunhao's Vue.js Project"/>
   </div>
 </template>
 
