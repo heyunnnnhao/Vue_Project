a3d757d78fc9fe2f1aa7ace7a4ca837761827872
unknown <heyunhaotq@outlook.com>
2020-09-04 00:59:23 +0800

    formatted

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,115 +1,112 @@
 <template>
-<div class='calculator'>
-  <main>
-    <input type="number" v-bind:value='current'>
-    <div class='keys'>
-      <button class='op__key' @click='clearAll'>AC</button>
-      <button class='op__key' @click='signn'>(-)</button>
-      <button class='op__key' @click='percent'>%</button>
-      <button class='op__key' @click='divide'>/</button>
-      <button class='num__key' @click='append("7")'>7</button>
-      <button class='num__key' @click='append("8")'>8</button>
-      <button class='num__key' @click='append("9")'>9</button>
-      <button class='op__key' @click='times'>x</button>
-      <button class='num__key' @click='append("4")'>4</button>
-      <button class='num__key' @click='append("5")'>5</button>
-      <button class='num__key' @click='append("6")'>6</button>
-      <button class='op__key' @click='minus'>-</button>
-      <button class='num__key' @click='append("1")'>1</button>
-      <button class='num__key' @click='append("2")'>2</button>
-      <button class='num__key' @click='append("2")'>3</button>
-      <button class='op__key' @click='add'>+</button>
-      <span></span>
-      <button class='num__key zero' @click='append("0")'>0</button>
-      <button class='num__key dot' @click='dot'>.</button>
-      <button class='op__key equal' @click='equal'>=</button>
-    </div>
-  </main>
-</div>
+  <div class="calculator">
+    <main>
+      <input type="number" v-bind:value="current" />
+      <div class="keys">
+        <button class='op__key' @click='clearAll'>AC</button>
+        <button class='op__key' @click='signn'>(-)</button>
+        <button class='op__key' @click='percent'>%</button>
+        <button class='op__key' @click='divide'>/</button>
+        <button class='num__key' @click='append("7")'>7</button>
+        <button class='num__key' @click='append("8")'>8</button>
+        <button class='num__key' @click='append("9")'>9</button>
+        <button class='op__key' @click='times'>x</button>
+        <button class='num__key' @click='append("4")'>4</button>
+        <button class='num__key' @click='append("5")'>5</button>
+        <button class='num__key' @click='append("6")'>6</button>
+        <button class='op__key' @click='minus'>-</button>
+        <button class='num__key' @click='append("1")'>1</button>
+        <button class='num__key' @click='append("2")'>2</button>
+        <button class='num__key' @click='append("2")'>3</button>
+        <button class='op__key' @click='add'>+</button>
+        <span></span>
+        <button class='num__key zero' @click='append("0")'>0</button>
+        <button class='num__key dot' @click='dot'>.</button>
+        <button class='op__key equal' @click='equal'>=</button>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Calculator',
-  data(){
-    return {  
+  name: "Calculator",
+  data() {
+    return {
       previous: null,
-      current: '0',
+      current: "0",
       operator: null,
       operatorClicked: false,
-      sign: '',
+      sign: "",
       end: false,
-    }
+    };
   },
   methods: {
     clearAll() {
-      this.current = '0'
+      this.current = "0";
     },
     signn() {
-      if(this.current != '')
-        this.current = this.current.charAt(0) === '-' ?
-          this.current.slice(1) : `-${this.current}`
+      if (this.current != "")
+        this.current =
+          this.current.charAt(0) === "-"
+            ? this.current.slice(1)
+            : `-${this.current}`;
     },
     percent() {
-      this.current = `${parseFloat(this.current) / 100}`
+      this.current = `${parseFloat(this.current) / 100}`;
     },
     append(number) {
-      this.current = '';
-      if(number == '0' && this.current == '')
-        this.current = ''
+      this.current = "";
+      if (number == "0" && this.current == "") this.current = "";
       else {
-        if(this.operatorClicked) {
-          this.current = ''
-          this.operatorClicked = false
+        if (this.operatorClicked) {
+          this.current = "";
+          this.operatorClicked = false;
         }
-        this.current = `${this.current}${number}`
+        this.current = `${this.current}${number}`;
       }
     },
     dot() {
-      if (this.current.indexOf('.') === -1)
-        this.append('.')
+      if (this.current.indexOf(".") === -1) this.append(".");
     },
     setPrevious() {
-      this.previous = this.current
+      this.previous = this.current;
       this.operatorClicked = true;
     },
     divide() {
-      this.operator = (a, b) => a / b
-      this.setPrevious()
-      this.sign = '÷'
+      this.operator = (a, b) => a / b;
+      this.setPrevious();
+      this.sign = "÷";
     },
     times() {
-      this.operator = (a, b) => a * b
-      this.setPrevious()
-      this.sign = 'x'
+      this.operator = (a, b) => a * b;
+      this.setPrevious();
+      this.sign = "x";
     },
     minus() {
-      this.operator = (a, b) => a - b
-      this.setPrevious()
-      this.sign = '-'
+      this.operator = (a, b) => a - b;
+      this.setPrevious();
+      this.sign = "-";
     },
     add() {
-      this.operator = (a, b) => a + b
-      this.setPrevious()
-      this.sign = '+'
+      this.operator = (a, b) => a + b;
+      this.setPrevious();
+      this.sign = "+";
     },
     equal() {
       this.current = this.operator(
         parseFloat(this.previous),
         parseFloat(this.current)
-      )
-      this.previous = null
-      this.sign = ''
-      this.end = true
+      );
+      this.previous = null;
+      this.sign = "";
+      this.end = true;
     },
     del() {
-      if(this.current)
-        this.current = this.current.slice(0, -1)
+      if (this.current) this.current = this.current.slice(0, -1);
     },
-
-  }
-}
+  },
+};
 </script>
 
 <style lang=scss scoped >
@@ -121,7 +118,7 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: 'JetBrain';
+  font-family: "JetBrain";
   font-size: 20px;
 }
 
@@ -154,7 +151,7 @@ main {
     -webkit-appearance: none;
   }
 
-  input[type=number] {
+  input[type="number"] {
     -moz-appearance: textfield;
   }
 
