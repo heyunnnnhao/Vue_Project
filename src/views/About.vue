c622d48b6dc03eadf03d15be852b87aee59900f9
heyunnnnhao <heyunhaotq@outlook.com>
2020-09-24 17:40:45 +0800

    about update

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,9 +2,9 @@
   <div class="about">
     <h3>This is an about page</h3>
     <p>This is a showcase gallery to demostrate some of my</p>
-      <p>mini projects redone using Vue.js</p>
+      <p>mini projects redone using Vue.js and hopefuly I can transform it to a wigdgt dasbopard</p>
       <h4>
-        <p>Stay simple. Stay naive</p>
+        <p>Still figuring out what am i gonna do lol</p>
       </h4>
   </div>
 </template>
