57a033c294ddc3feb42c039814a1d8e6405407ba
heyunnnnhao <heyunhaotq@outlook.com>
2020-09-26 18:33:12 +0800

    wording

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h3>This is an about page</h3>
-    <p>This is a showcase gallery to demostrate some of my</p>
+    <p>This is a showcase project to demostrate some of my</p>
       <p>mini projects redone using Vue.js and hopefuly I can transform it to a wigdgt dasbopard</p>
       <h4>
         <p>Still figuring out what am i gonna do lol</p>
